ab54089cebc39ffdd09ba9d0230d5b0b1ab146fe
Kristina Gromova <christina28romova@yandex.ru>
2020-05-26 11:55:20 +0300

    add middleware for like/dislike

--- routes/cards.js
+++ routes/cards.js
@@ -6,6 +6,6 @@ router.get('/', cardController.findCards);
 router.get('/:cardId', cardController.getCardMiddeleware, cardController.findCardById);
 router.delete('/:cardId', cardController.getCardMiddeleware, cardController.deleteCard);
 router.post('/', cardCreateValidator, cardController.createCard);
-router.put('/:cardId/likes', cardController.likeCard);
-router.delete('/:cardId/likes', cardController.dislikeCard);
+router.put('/:cardId/likes', cardController.getCardMiddeleware, cardController.likeCard);
+router.delete('/:cardId/likes', cardController.getCardMiddeleware, cardController.dislikeCard);
 module.exports = router;
